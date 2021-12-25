de528066bfdf2556853b3ad9c68740ca4f79ebe4
tintin3274 <tintin3274@hotmail.co.th>
2021-12-25 21:27:32 +0700

    Initial commit

---
+++ src/main/java/th/ku/orderme/OrderMeApplication.java
@@ -0,0 +1,13 @@
+package th.ku.orderme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/th/ku/orderme/OrderMeApplicationTests.java
@@ -0,0 +1,13 @@
+package th.ku.orderme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
